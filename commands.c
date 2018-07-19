0f183330b7708813c0d55f661da7954736cdbc2f
tpiven <35926102+tpiven@users.noreply.github.com>
2018-07-19 21:53:46 +0300

    + add and sub

--- commands.c
+++ commands.c
@@ -15,8 +15,10 @@
 
 void ft_live(t_pc *pc, t_union *un)
 {
+    int num_player;
+    
     pc->alive = 1;
-    int num_player = ft_get_int(un, pc->curr_position + 1, 4);
+    num_player = ft_get_int(un, pc->curr_position + 1, 4);
     while (un->bot->next != NULL)
     {
         if (un->bot->id == num_player)
@@ -30,6 +32,10 @@ void ft_live(t_pc *pc, t_union *un)
 void ft_load(t_pc *pc, t_union *un)
 {
     unsigned int n;
+    
+    if (un->map[pc->curr_position + 3].value > 16)
+        return ;
+    
     ft_check_codage(un->map[pc->curr_position + 1].value, pc);
     if (pc->arg[0] == DIR_CODE)
     {
@@ -44,4 +50,78 @@ void ft_load(t_pc *pc, t_union *un)
         pc->carry = 1;
     else
         pc->carry = 0;
+}
+
+
+void ft_st(t_pc *pc, t_union *un)
+{
+    int position;
+    uint8_t *num;
+    uint8_t one;
+    uint8_t two;
+    
+    
+    one = un->map[pc->curr_position + 2].value;
+    if (one > 16)
+        return ;
+    ft_check_codage(un->map[pc->curr_position + 1].value, pc);
+    if (pc->arg[1] == REG_CODE)
+    {
+        two = un->map[pc->curr_position + 3].value;
+        if (two > 16)
+            return ;
+        pc->reg[two] = pc->reg[one];
+    }
+    if (pc->arg[1] == IND_CODE)
+    {
+        position = pc->curr_position + ((short)ft_get_int(un, pc->curr_position + 1, 2) % IDX_MOD);
+        num = ft_get_char_from_int(pc, pc->reg[one]);
+        un->map[position].value = num[0];
+        un->map[position + 1].value = num[1];
+        un->map[position + 2].value = num[2];
+        un->map[position + 3].value = num[3];
+    }
+}
+
+void ft_add(t_pc *pc, t_union *un)
+{
+    uint8_t one;
+    uint8_t two;
+    uint8_t three;
+    unsigned int n;
+    one = un->map[pc->curr_position + 2].value;
+    two = un->map[pc->curr_position + 3].value;
+    three = un->map[pc->curr_position + 4].value;
+    if (one < 16 && two < 16 && three < 16)
+    {
+        n = pc->reg[one] + pc->reg[two];
+        pc->reg[three] = n;
+        
+        if (n == 0)
+            pc->carry = 1;
+        else
+            pc->carry = 0;
+    }
+}
+
+void ft_sub(t_pc *pc, t_union *un)
+{
+    uint8_t one;
+    uint8_t two;
+    uint8_t three;
+    unsigned int n;
+    
+    one = un->map[pc->curr_position + 2].value;
+    two = un->map[pc->curr_position + 3].value;
+    three = un->map[pc->curr_position + 4].value;
+    if (one < 16 && two < 16 && three < 16)
+    {
+        n = pc->reg[one] - pc->reg[two];
+        pc->reg[three] = n;
+        
+        if (n == 0)
+            pc->carry = 1;
+        else
+            pc->carry = 0;
+    }
 }
