ae23be9fc746689267ec954d715d974dfb71e68d
tpiven <35926102+tpiven@users.noreply.github.com>
2018-07-26 15:55:30 +0300

    update

--- src/display_map.c
+++ src/display_map.c
@@ -1,95 +1,41 @@
-
-
 #include "../inc/vm.h"
 
-void 		move_code_to_map(t_union *un, int color)
-{
-	int i;
-	int j;
-	int move;
-	t_bot	*bot;
 
-	j = 0;
-	un->pc = NULL;
-	bot = un->bot;
-	move = 0;
-	while (bot)
-	{
-		un->pc = pc_push_front(un->pc,pc_new(j + move, bot->id));
-
-		i = 0;
-		while (i < bot->code_length)
-		{
-			un->map[j + move].value = bot->code[i];
-			un->map[j + move].color = color;
-			if (i == 0)
-			{
-				un->map[j + move].cursor = 1;
-				un->pc->curr_command = un->map[j + move].value;
-			}
-			++i;
-			++j;
-		}
-		move += 682 - bot->code_length + 100;
-		++color;
-		bot = bot->next;
-	}
-}
-
-
-void		update_pc(t_union *un)
-{
-	int		i;
-	t_pc	*pc;
-
-	i = 0;
-	while (i < MEM_SIZE)
-	{
-		un->map[i].cursor = 0;
-		++i;
-	}
-	pc = un->pc;
-	while (pc)
-	{
-		un->map[pc->curr_position].cursor = 1;
-		pc = pc->next;
-	}
-
-}
-
-void		display_map(t_union *un)
-{
-	int key;
-	int i;
-	int k;
+void		display_map(t_union *un) {
+    int key;
+    int i;
+    int k;
     WINDOW *win;
 
-	key = 1;
-	initscr();
-	raw();
-	nodelay(stdscr, TRUE);
-	noecho();
-	curs_set(0);
-    int yMax, xMax;
-    getmaxyx(stdscr, yMax, xMax);
-	start_color();
-    win = newwin(yMax - 2, xMax - 2, 1, 1);
-	init_pair(9,  COLOR_BLACK,COLOR_WHITE);
-	init_pair(1, COLOR_WHITE, COLOR_BLACK);
-	init_pair(2, COLOR_RED, COLOR_BLACK);
-	init_pair(3, COLOR_BLUE, COLOR_BLACK);
-	init_pair(5, COLOR_YELLOW, COLOR_BLACK);
-	init_pair(4, COLOR_MAGENTA, COLOR_BLACK);
-	init_pair(11, COLOR_WHITE, COLOR_CYAN);
-	init_pair(19, COLOR_GREEN, COLOR_GREEN);
-	init_pair(12, COLOR_WHITE, COLOR_RED);
-	init_pair(13, COLOR_WHITE, COLOR_BLUE);
-	init_pair(14, COLOR_WHITE, COLOR_MAGENTA);
-	init_pair(15, COLOR_WHITE, COLOR_YELLOW);
+    key = 1;
+    initscr();
+    raw();
+    nodelay(stdscr, TRUE);
+    noecho();
+    curs_set(0);
+    int yMax = 200;
+    int xMax = 200;
 
 
-	while (key)
-	{
+
+    // getmaxyx(stdscr, yMax, xMax);
+    start_color();
+    win = newwin(yMax, xMax, 1, 1);
+    init_pair(9, COLOR_BLACK, COLOR_WHITE);
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_RED, COLOR_BLACK);
+    init_pair(3, COLOR_BLUE, COLOR_BLACK);
+    init_pair(5, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(4, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(11, COLOR_WHITE, COLOR_CYAN);
+    init_pair(19, COLOR_GREEN, COLOR_GREEN);
+    init_pair(12, COLOR_WHITE, COLOR_RED);
+    init_pair(13, COLOR_WHITE, COLOR_BLUE);
+    init_pair(14, COLOR_WHITE, COLOR_MAGENTA);
+    init_pair(15, COLOR_WHITE, COLOR_YELLOW);
+
+
+    while (key) {
 
         i = 0;
         k = 0;
@@ -98,28 +44,29 @@ void		display_map(t_union *un)
 
 
         /* Start color 			*/
-        while (i < MEM_SIZE)
-        {
+        mvprintw(5, xMax + 5, "Cycle: %d\n", un->cycle);
+        mvprintw(8, xMax + 5, "Cycle_to_die: %d\n", un->cycle_to_die);
+        mvprintw(12, xMax + 5, "Number live: %d\n", un->bot->num_live);
+        mvprintw(15, xMax + 5, "Last live: %d\n", un->bot->last_live);
+
+        while (i < MEM_SIZE) {
             wattron(win, COLOR_PAIR(un->map[i].color));
-            if (un->map[i].cursor)
-            {
+            if (un->map[i].cursor) {
                 attron(A_BOLD);
                 wattron(win, COLOR_PAIR(un->map[i].color + 10));
             }
 
             mvwprintw(win, y, x, "%02x ", un->map[i].value);
-            x +=3;
+            x += 3;
 
             wattroff(win, COLOR_PAIR(un->map[i].color));
             wprintw(win, " ");
-            if (un->map[i].cursor)
-            {
+            if (un->map[i].cursor) {
                 wattroff(win, A_BOLD);
                 wattroff(win, COLOR_PAIR(un->map[i].color + 10));
             }
             ++k;
-            if (k == 64)
-            {
+            if (k == 64) {
                 y++;
                 x = 0;
                 k = 0;
@@ -130,21 +77,22 @@ void		display_map(t_union *un)
         refresh();
         key = getch();
         if (key == 27)
-            break ;
-        if (key == 32)
-        {
-            while (1)
-            {
+            break;
+        if (key == 32) {
+            while (1) {
                 key = getch();
                 if (key == 32)
-                    break ;
+                    break;
             }
         }
-        usleep(100000);
+        if (!un->pc || un->cycle_to_die <= 0)
+            break;
+        usleep(10000);
         corewar(un);
         update_pc(un);
         //clear();
-}
-	endwin();
-	//curs_set(1);
+    }
+
+    //endwin();
+    //curs_set(1);
 }
